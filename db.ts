0524d555259d420dcf378fbf1616c81adfefb200
Chris Troutner <chris.troutner@gmail.com>
2022-01-02 10:56:28 -0800

    Increasing connection timeout from default of 10 sec to 120 sec

--- db.ts
+++ db.ts
@@ -19,7 +19,7 @@ export class Db {
 
     private async checkClientStatus(): Promise<boolean> {
         if (!this.mongo) {
-            this.mongo = await MongoClient.connect(this.dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+            this.mongo = await MongoClient.connect(this.dbUrl, { useNewUrlParser: true, useUnifiedTopology: true, connectTimeoutMS: 120000 });
             this.db = this.mongo.db(this.dbName);
             return true;
         }
